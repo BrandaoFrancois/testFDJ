51629e4db0fdb6823b46b5953e13081d4293f457
BFX <francois.xavier.brandao@gmail.com>
2024-03-20 09:27:18 +0100

    Adding Unit Test about the Service part

--- app/src/test/java/com/test/fdj/service/TeamServiceImplTest.kt
+++ app/src/test/java/com/test/fdj/service/TeamServiceImplTest.kt
@@ -1,15 +1,94 @@
 package com.test.fdj.service
 
-/*
- TODO En cours, il ne reste que ça
- */
+import com.test.fdj.data.TeamElement
+import com.test.fdj.provider.SportsDataProvider
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Test
+
 class TeamServiceImplTest {
 
-/*    @org.junit.jupiter.api.Test
-    fun getOddTeamListForLeagueSortedAnalphabeticaly() {
+    private val sportsDataProvider = object: SportsDataProvider {
+        override suspend fun provideTeamsOfLeague(leagueName: String): List<TeamElement> {
+            return listOf(
+                TeamElement("A", ""),
+                TeamElement("B", ""),
+                TeamElement("C", ""),
+                TeamElement("D", ""),
+                TeamElement("E", ""),
+                TeamElement("F", ""),
+                TeamElement("G", "")
+            )
+        }
+
+        override suspend fun provideLeagues(): List<String> {
+            return listOf(
+                "a",
+                "alpa",
+                "alpha",
+                "alphabet",
+                "b",
+                "ba",
+                "england",
+                "England",
+                "ENgland"
+            )
+        }
     }
 
-    @org.junit.jupiter.api.Test
-    fun getLeaguesFilteredBy() {
-    }*/
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun testGettingOddTeamListForLeagueSortedAnalphabeticaly() = runTest {
+        val leagueNameToTest = ""
+        val testDispatcher = UnconfinedTestDispatcher(testScheduler)
+        val testService = TeamServiceImpl(sportsDataProvider, testDispatcher)
+        lateinit var resultToTestFormatted : String
+
+        launch {
+            val resultToTest = testService.getOddTeamListForLeagueSortedAnalphabeticaly(leagueNameToTest)
+            resultToTestFormatted = resultToTest.joinToString(separator = "") { it.name }
+        }
+        advanceUntilIdle()
+
+        Assert.assertEquals("GECA", resultToTestFormatted)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun testGettingLeaguesFilteredByWithCaseInsensitive() = runTest {
+        val filterString = "eN"
+        val testDispatcher = UnconfinedTestDispatcher(testScheduler)
+        val testService = TeamServiceImpl(sportsDataProvider, testDispatcher)
+        lateinit var resultToTestFormatted : String
+
+        launch {
+            val resultToTest = testService.getLeaguesFilteredBy(filterString)
+            resultToTestFormatted = resultToTest.joinToString(separator = "") { it }
+        }
+        advanceUntilIdle()
+
+        Assert.assertEquals("englandEnglandENgland", resultToTestFormatted)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun testGettingLeaguesFilteredByWithNormalString() = runTest {
+        val filterString = "alpha"
+        val testDispatcher = UnconfinedTestDispatcher(testScheduler)
+        val testService = TeamServiceImpl(sportsDataProvider, testDispatcher)
+        lateinit var resultToTestFormatted : String
+
+        launch {
+            val resultToTest = testService.getLeaguesFilteredBy(filterString)
+            resultToTestFormatted = resultToTest.joinToString(separator = "") { it }
+        }
+        advanceUntilIdle()
+
+        Assert.assertEquals("alphaalphabet", resultToTestFormatted)
+    }
+
 }
